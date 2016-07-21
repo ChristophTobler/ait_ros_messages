4ad0abd8eb8b351581e79ba5f611f247d959a475
ChristophTobler <toblech@student.ethz.ch>
2016-07-21 14:22:45 +0200

    addaption for new omni msgs

--- scripts/omni_vio_sensor_republisher.py
+++ scripts/omni_vio_sensor_republisher.py
@@ -47,49 +47,72 @@ import numpy as np
 import genpy
 
 
-def vio_sensor_cb1(data):
-    global cam0_pub, cam1_pub
+uvc_node_name = "/uvc_camera"
+init_0 = 0
+init_1 = 0
+init_2 = 0
+init_3 = 0
+init_4 = 0
+
+def vio_sensor_cb0(data):
+    global cam0_pub, cam1_pub, init_0
+    if init_0 == 0:
+        cam0_pub = rospy.Publisher(uvc_node_name + "/cam_0/image_raw", Image, queue_size=1)
+        cam1_pub = rospy.Publisher(uvc_node_name + "/cam_1/image_raw", Image, queue_size=1)
+        init_0 = 1
 
     cam0_pub.publish(data.left_image)
     cam1_pub.publish(data.right_image)
 
-def vio_sensor_cb2(data):
-    global cam2_pub, cam3_pub
+def vio_sensor_cb1(data):
+    global cam2_pub, cam3_pub, init_1
+    if init_1 == 0:
+        cam2_pub = rospy.Publisher(uvc_node_name + "/cam_2/image_raw", Image, queue_size=1)
+        cam3_pub = rospy.Publisher(uvc_node_name + "/cam_3/image_raw", Image, queue_size=1)
+        init_1 = 1
 
     cam2_pub.publish(data.left_image)
     cam3_pub.publish(data.right_image)
 
-def vio_sensor_cb3(data):
-    global cam4_pub, cam5_pub
+def vio_sensor_cb2(data):
+    global cam4_pub, cam5_pub, init_2
+    if init_2 == 0:
+        cam4_pub = rospy.Publisher(uvc_node_name + "/cam_4/image_raw", Image, queue_size=1)
+        cam5_pub = rospy.Publisher(uvc_node_name + "/cam_5/image_raw", Image, queue_size=1)
+        init_2 = 1
 
     cam4_pub.publish(data.left_image)
     cam5_pub.publish(data.right_image)
 
-def vio_sensor_cb4(data):
-    global cam6_pub, cam7_pub
+def vio_sensor_cb3(data):
+    global cam6_pub, cam7_pub, init_3
+    if init_3 == 0:
+        cam6_pub = rospy.Publisher(uvc_node_name + "/cam_6/image_raw", Image, queue_size=1)
+        cam7_pub = rospy.Publisher(uvc_node_name + "/cam_7/image_raw", Image, queue_size=1)
+        init_3 = 1
 
     cam6_pub.publish(data.left_image)
     cam7_pub.publish(data.right_image)
+
+def vio_sensor_cb4(data):
+    global cam8_pub, cam9_pub, init_4
+    if init_4 == 0:
+        cam0_pub = rospy.Publisher(uvc_node_name + "/cam_8/image_raw", Image, queue_size=1)
+        cam1_pub = rospy.Publisher(uvc_node_name + "/cam_9/image_raw", Image, queue_size=1)
+        init_4 = 1
+
+    cam8_pub.publish(data.left_image)
+    cam9_pub.publish(data.right_image)
 
 
 if __name__ == "__main__":
 
     rospy.init_node('image_publisher')
 
-    cam0_pub = rospy.Publisher("cam0_image", Image, queue_size=1)
-    cam1_pub = rospy.Publisher("cam1_image", Image, queue_size=1)
-    rospy.Subscriber("/vio_sensor_1", VioSensorMsg, vio_sensor_cb1, queue_size=1)
-
-    cam2_pub = rospy.Publisher("cam2_image", Image, queue_size=1)
-    cam3_pub = rospy.Publisher("cam3_image", Image, queue_size=1)
-    rospy.Subscriber("/vio_sensor_2", VioSensorMsg, vio_sensor_cb2, queue_size=1)
-
-    cam4_pub = rospy.Publisher("cam4_image", Image, queue_size=1)
-    cam5_pub = rospy.Publisher("cam5_image", Image, queue_size=1)
-    rospy.Subscriber("/vio_sensor_3", VioSensorMsg, vio_sensor_cb3, queue_size=1)
-
-    cam6_pub = rospy.Publisher("cam6_image", Image, queue_size=1)
-    cam7_pub = rospy.Publisher("cam7_image", Image, queue_size=1)
-    rospy.Subscriber("/vio_sensor_4", VioSensorMsg, vio_sensor_cb4, queue_size=1)
+    rospy.Subscriber(uvc_node_name + "/vio_sensor_0", VioSensorMsg, vio_sensor_cb0, queue_size=1)
+    rospy.Subscriber(uvc_node_name + "/vio_sensor_1", VioSensorMsg, vio_sensor_cb1, queue_size=1)
+    rospy.Subscriber(uvc_node_name + "/vio_sensor_2", VioSensorMsg, vio_sensor_cb2, queue_size=1)
+    rospy.Subscriber(uvc_node_name + "/vio_sensor_3", VioSensorMsg, vio_sensor_cb3, queue_size=1)
+    rospy.Subscriber(uvc_node_name + "/vio_sensor_4", VioSensorMsg, vio_sensor_cb4, queue_size=1)
 
     rospy.spin()
